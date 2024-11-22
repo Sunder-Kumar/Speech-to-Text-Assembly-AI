b8d0c8bb80f058544f05a8aa39dff0f92714eb14
Sunder Kumar <skmalhi157@gmail.com>
2024-11-23 02:06:48 +0500

    Fixed deployment issues

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ def transcription(transcript_id):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
